9e576d6b4f37752464ff07efaeb46e0792c3f6ac
Nikhil Wasnik <nikhil.wasnik@ltimindtree.com>
2025-01-20 17:49:04 +0530

    route testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 
 const port = process.env.PORT || 3000; 
 app.get('/', (req, res) => { 
-    res.send('Hello, world!'); 
+    res.send('Welcome to LTIM blogs!'); 
 }); 
 app.listen(port, () => { 
     console.log(`Server is running on port ${port}`); 
